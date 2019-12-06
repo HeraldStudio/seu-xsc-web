03dee089ee1bc52606dd5ebe7810f05bc7dbb204
R_shrug <656598766@qq.com>
2019-12-07 02:28:09 +0800

    节假日行程登记 2019-12-7 by.rlh

--- src/router/index.js
+++ src/router/index.js
@@ -33,6 +33,23 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/StudentGrade.vue')
+  },
+  {
+    path: '/holiday-trip-register',
+    name: 'holiday-trip-register',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/HolidayTripRegister.vue'),
+    // beforeEnter: (to, from, next) => {
+    //   let isHoliday = true
+    //   if(!isHoliday){
+        
+    //     next('/home')
+    //   }else{
+    //     next()
+    //   }
+    // }
   }
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
             <menu-item :icon="basicInfoIcon" title="学生基本信息速查" route="/student-basic-info" role="辅导员/学生处"></menu-item>
             <menu-item :icon="curriculumIcon" title="学生课表查询" role="辅导员/学生处"></menu-item>
             <menu-item :icon="gradeIcon" title="学生成绩查询" route="/student-grade" role="辅导员/学生处"></menu-item>
-            <menu-item :icon="outingIcon" title="节假日行程登记" role="学生"></menu-item>
+            <menu-item :icon="outingIcon" title="节假日行程登记" route="/holiday-trip-register" role="学生"></menu-item>
             <menu-item :icon="takeLeaveIcon" title="请假登记" role="学生"></menu-item>
             <menu-item :icon="takeLeaveIcon" title="请假审批" role="辅导员"></menu-item>
             <menu-item :icon="outingIcon" title="离/在校情况查询" role="辅导员/学生处"></menu-item>
@@ -28,7 +28,27 @@ export default {
         'menu-item':MenuItem,
     },
     data(){
-        return {outingIcon, takeLeaveIcon, basicInfoIcon, locateIcon, curriculumIcon, gradeIcon}
+        return {
+            outingIcon, 
+            takeLeaveIcon, 
+            basicInfoIcon, 
+            locateIcon, 
+            curriculumIcon, 
+            gradeIcon,
+            isHoliday: true
+        }
+    },
+    methods: {
+        // toHolidayTripRegister() {
+        //     if(this.isHoliday){
+        //         this.$router.push('/holiday-trip-register')
+        //     }else{
+        //         this.$message({
+        //             message: '警告哦，这是一条警告消息',
+        //             type: 'warning'
+        //             })
+        //     }
+        // }
     }
 }
 </script>
